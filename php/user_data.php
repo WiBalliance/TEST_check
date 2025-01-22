7f2d7fc9adced45663792cae93b4e54e41505850
WiBalliance <njkhgwertnmbdr@gmail.com>
2025-01-23 01:06:14 +0900

    Add files via upload

--- php/user_data.php
+++ php/user_data.php
@@ -1,16 +1,11 @@
 <?php
-// 仮のデータ（本番環境ではデータベースを使用）
 $users = [
     ["id" => "001", "name" => "山田太郎"],
     ["id" => "002", "name" => "佐藤花子"],
     ["id" => "003", "name" => "田中一郎"],
-    ["id" => "004", "name" => "鈴木二郎"],
 ];
 
 foreach ($users as $user) {
-    echo "<tr>";
-    echo "<td>" . htmlspecialchars($user["id"]) . "</td>";
-    echo "<td>" . htmlspecialchars($user["name"]) . "</td>";
-    echo "</tr>";
+    echo "<tr><td>{$user['id']}</td><td>{$user['name']}</td></tr>";
 }
 ?>
