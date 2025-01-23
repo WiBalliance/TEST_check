5ff9c55d631ed17f52b9a13898fe908b07381ef2
WiBalliance <njkhgwertnmbdr@gmail.com>
2025-01-23 20:13:36 +0900

    Add files via upload

--- JavaScript/script_schedule_events.js
+++ JavaScript/script_schedule_events.js
@@ -99,9 +99,12 @@ const loadTasks = async () => {
     // 
     "../tasks/tasks_turi.json",               //釣り穴選手権
     // 超得イベント
-    "../tasks/tasks_setugenboueki.json",  　　//雪原貿易
+    "../tasks/tasks_setugenboueki.json",      //雪原貿易
     // 通常イベント
-    "../tasks/tasks_hyougennsihaisya.json",　 //氷原支配者
+    "../tasks/tasks_hyougennsihaisya.json",   //氷原支配者
+    "../tasks/tasks_SvS.json",                //SvS
+    "../tasks/tasks_rekkanokiba.json",        //烈火の牙
+    "../tasks/tasks_youheinomeiyo.json",      //傭兵の名誉
     "../tasks/tasks_gunbi.json",              //軍備競技
     "../tasks/tasks_sikannkeikaku.json",      //士官計画
     "../tasks/tasks_doumeisoudouin.json",     //同盟総動員
