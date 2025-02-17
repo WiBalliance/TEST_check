4c743350a8242df7bdd61b1e4065873881a8ea27
WiBalliance <njkhgwertnmbdr@gmail.com>
2025-02-18 07:06:58 +0900

    Update script_schedule_events.js

--- JavaScript/script_schedule_events.js
+++ JavaScript/script_schedule_events.js
@@ -18,34 +18,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const loadTasks = async (calendar, listEl) => {
   const taskFiles = [
-    "../tasks/tasks_old.json",
-    "../tasks/tasks_hyouketsunohihou.json",
-    "../tasks/tasks_kisetu_events.json",
-    "../tasks/tasks_turi.json",
-    "../tasks/tasks_setugenboueki.json",
-    "../tasks/tasks_mia.json",
-    "../tasks/tasks_hyougennsihaisya.json",
-    "../tasks/tasks_zengunsansen.json",
-    "../tasks/tasks_taiyoujou.json",
-    "../tasks/tasks_SvS.json",
-    "../tasks/tasks_setugen_doumeidaisakusen.json",
-    "../tasks/tasks_joe.json",
-    "../tasks/tasks_takenoko.json",
-    "../tasks/tasks_yajyu.json",
-    "../tasks/tasks_rekkanokiba.json",
-    "../tasks/tasks_youheinomeiyo.json",
-    "../tasks/tasks_gunbi.json",
-    "../tasks/tasks_sikannkeikaku.json",
-    "../tasks/tasks_doumeisoudouin.json",
-    "../tasks/tasks_doumeisouha.json",
-    "../tasks/tasks_toride.json",
-    "../tasks/tasks_heiki.json",
-    "../tasks/tasks_kyoukoku.json",
-    "../tasks/tasks_jina.json",
-    "../tasks/tasks_herbester.json",
-    "../tasks/tasks_akatsukinotenbou.json",
-    "../tasks/tasks_kuma1.json",
-    "../tasks/tasks_kuma2.json"
+    // 終了イベント
+    "../tasks/tasks_old.json",                       //終了したイベント
+    // 秘宝イベント
+    "../tasks/tasks_hyouketsunohihou.json",          //氷結の秘宝
+    // 季節イベント
+    "../tasks/tasks_kisetu_events.json",             //季節もののイベント
+    // 
+    "../tasks/tasks_turi.json",                      //釣り穴選手権
+    // 超得イベント
+    "../tasks/tasks_setugenboueki.json",             //雪原貿易
+    "../tasks/tasks_mia.json",                       //ミアの占い屋
+    // 通常イベント
+    "../tasks/tasks_hyougennsihaisya.json",          //氷原支配者
+    "../tasks/tasks_zengunsansen.json",              //全軍参戦
+    "../tasks/tasks_taiyoujou.json",                 //王城決戦
+    "../tasks/tasks_SvS.json",                       //SvS
+    "../tasks/tasks_setugen_doumeidaisakusen.json",  //雪原貿易、同盟大作戦
+    "../tasks/tasks_joe.json",                       //クレイジー・ジョイ
+    "../tasks/tasks_takenoko.json",                  //燃霜鉱区
+    "../tasks/tasks_yajyu.json",                     //野獣駆逐
+    "../tasks/tasks_rekkanokiba.json",               //烈火の牙
+    "../tasks/tasks_youheinomeiyo.json",             //傭兵の名誉
+    "../tasks/tasks_gunbi.json",                     //軍備競技
+    "../tasks/tasks_sikannkeikaku.json",             //士官計画
+    "../tasks/tasks_doumeisoudouin.json",            //同盟総動員
+    "../tasks/tasks_doumeisouha.json",               //同盟争覇戦
+    "../tasks/tasks_toride.json",                    //要塞・砦争奪戦
+    "../tasks/tasks_heiki.json",                     //兵器工場争奪戦
+    "../tasks/tasks_kyoukoku.json",                  //峡谷合戦
+    "../tasks/tasks_jina.json",                      //ジーナの復讐
+    "../tasks/tasks_herbester.json",                 //ハーベスター
+    "../tasks/tasks_akatsukinotenbou.json",          //暁の展望
+    "../tasks/tasks_kuma1.json",                     //熊罠1
+    "../tasks/tasks_kuma2.json"                      //熊罠2
   ];
 
   try {
