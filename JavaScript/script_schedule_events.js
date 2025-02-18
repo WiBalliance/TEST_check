fd484438ed9f1a9c33fad5112d6c211b363d45a8
WiBalliance <njkhgwertnmbdr@gmail.com>
2025-02-18 12:03:19 +0900

    Update script_schedule_events.js

--- JavaScript/script_schedule_events.js
+++ JavaScript/script_schedule_events.js
@@ -1,10 +1,3 @@
-import React, { useEffect, useState } from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import listPlugin from "@fullcalendar/list";
-import interactionPlugin from "@fullcalendar/interaction";
-
 const CalendarComponent = () => {
   const [events, setEvents] = useState([]);
 
